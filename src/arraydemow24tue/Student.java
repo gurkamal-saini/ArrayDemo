97dab6b407ad8fbd2c2c16ee2ed4dc0ab5ba4373
Gurkamal Singh <sin14371@shernet.sheridancollege.ca>
2024-01-30 11:54:06 -0500

    added a variable in student

--- src/arraydemow24tue/Student.java
+++ src/arraydemow24tue/Student.java
@@ -11,7 +11,12 @@ package arraydemow24tue;
 public class Student {
     private int sid;
     private String sname;
+    private String Address;
 
+    public String getAddress()  {
+        return Address;
+    }
+    
     public Student(int sid, String sname) {
         this.sid = sid;
         this.sname = sname;
